a8ed2a9d2c846016e89537d53550b423594604cf
christopheluc62 <christopheluc62@gmail.com>
2021-03-28 22:13:05 +0200

    html css js modifications

--- loterie.js
+++ loterie.js
@@ -1,12 +1,44 @@
+function createCase(number) {
+	const container = document.querySelector(".container-grid");
+	const ccase = document.createElement("div");
+	ccase.classList.add("ccase-class");
+	container.appendChild(ccase);
+
+	const ccaseNumber = document.createElement("h2");
+  	ccaseNumber.classList.add("ccaseNumber-class");
+	ccaseNumber.innerHTML = `${number}`;
+	ccase.appendChild(ccaseNumber);
+}
+
+for (let i=2; i<=25; i++) {
+	createCase(i);
+}
+
 function entierAleatoire(min, max) {
  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 let array = [];
 for (let i=0; i<5; i++) {
-	let number = entierAleatoire(1, 50);
+	let number = entierAleatoire(1, 25);
 	array.push(number);
 }
-//array.sort() ?? voir si on classe les numéros dans l'ordre
-console.log(array);
+document.getElementById("resultat").innerHTML = array.join("-");
+
+
 //vérifier ensuite qu'il n'y a pas de numéros identiques
+
+//pour trier dans l'ordre croissant
+// const array=[4,12,68,0,1,56,23,4];
+
+//   function compare(x, y) {
+//     return x - y;
+//   }
+// let nombres = array;
+// nombres.sort(compare);
+
+// if(nombres === array) {
+// console.log("true");
+// } else {
+// console.log("false");
+// }
